6e175945d0c6d44de43212d47c81942af6f30d92
Jarrett Croll <jarrett.croll@gmail.com>
2013-07-10 17:12:11 +0000

    Added guzzle service operations

--- Client/FoursquareClient.php
+++ Client/FoursquareClient.php
@@ -6,12 +6,13 @@ use Guzzle\Common\Collection;
 use Guzzle\Plugin\Oauth\OauthPlugin;
 use Guzzle\Service\Client;
 use Guzzle\Service\Description\ServiceDescription;
+use Guzzle\Service\Builder\ServiceBuilder;
 
 class FoursquareClient extends Client
 {
     public static function factory($config = array())
     {
-        $default = array('base_url' => 'https://api.foursquare.com/v2');
+        $default = array('base_url' => 'https://api.foursquare.com/v2/');
 
         $required = array(
             'client_id',
@@ -22,6 +23,7 @@ class FoursquareClient extends Client
 
         $client = new self($config->get('base_url'), $config);
         $client->setDefaultOption('query',  $config);
+        $client->setDescription(ServiceDescription::factory(__DIR__.'/../Resources/config/client.json'));
 
         return $client;
     }
